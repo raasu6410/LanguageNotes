99fdc74fd7d9da04cc31c171c5ad3051989743a4
knk-naviin <knknaveen28@gmail.com>
2021-10-08 10:57:30 +0530

    bottom navigation bar

--- language_notes/lib/main.dart
+++ language_notes/lib/main.dart
@@ -1,27 +1,89 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-void main(){
+
+void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Remainder(),
   ));
 }
-class Remainder extends StatefulWidget {
-  const Remainder({Key? key}) : super(key: key);
 
+class Remainder extends StatefulWidget {
+  int _selectedindex = 0;
   @override
   _RemainderState createState() => _RemainderState();
 }
+int _selectedIndex = 0;
+
+void _onItemTapped(int index) {
+ setStat
+}
 
 class _RemainderState extends State<Remainder> {
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+
+    ),
+    Text(
+      'Index 1: Business',
+
+    ),
+    Text(
+      'Index 2: School',
+
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        centerTitle: true,
-        title: Text ('Language Notes'),
+      backgroundColor: Colors.white,
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: (){},
+            tooltip: "Edit",
+            icon: Icon(Icons.edit),
+          ),
+          centerTitle: true,
+          title: Text('Language Notes'),
+        ),
+      bottomNavigationBar: BottomNavigationBar(
+
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Colors.white,
+            label: "Home",
+              icon: Icon(Icons.home)
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.white,
+            label: "Settings",
+              icon: Icon(Icons.settings)
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.white,
+            label: "Profile",
+              icon: Icon(Icons.person)
+          )
+        ],
       ),
-      body:Container(
-child: Text('Edit'),
-      )
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CupertinoSearchTextField(
+
+            ),
+          ),
+          
+        ],
+      ),
     );
   }
 }
